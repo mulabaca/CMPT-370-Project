d9d680a094a379b1e1119597fe0add8c37b4db2b
Reyes <wherismyboard@gmail.com>
2023-12-06 04:30:08 -0700

    colision, and item pickup

--- WebGLTemplateNov162021/src/game.js
+++ WebGLTemplateNov162021/src/game.js
@@ -3,7 +3,8 @@ class Game {
         this.state = state;
         this.spawnedObjects = [];
         this.collidableObjects = [];
-        this.state.trains = []
+        this.state.trains = [];
+        this.collectibles = [];
 
         this.direction = Object.freeze({
             NORTH: 0,
@@ -21,33 +22,70 @@ class Game {
         this.CAMERA_HEIGHT = 5;
     }
 
-    // example - we can add our own custom method to our game and call it using 'this.customMethod()'
-    customMethod() {
-        console.log("Custom method!");
-    }
+    
 
     // example - create a collider on our object with various fields we might need (you will likely need to add/remove/edit how this works)
-    // createSphereCollider(object, radius, onCollide = null) {
-    //     object.collider = {
-    //         type: "SPHERE",
-    //         radius: radius,
-    //         onCollide: onCollide ? onCollide : (otherObject) => {
-    //             console.log(`Collided with ${otherObject.name}`);
-    //         }
-    //     };
-    //     this.collidableObjects.push(object);
-    // }
+    createTrainCollider(object, radius, onCollide = null) {
+        object.collider = {
+            type: "TRAIN",
+            radius: radius,
+            onCollide: onCollide ? onCollide : (otherObject) => {
+                if(otherObject.name.includes("train") || otherObject.name.includes("oil_tank")){
+                    if (((object.previous == null) || (otherObject.name != this.getObject(object.previous).name)) && 
+                    ((object.next == null) || (otherObject.name != this.getObject(object.next).name))) {
+                        console.log("train Collision!");
+                        this.kill(object);
+                        this.kill(otherObject);
+                    }
+                }
+                
+            }
+        };
+        this.collidableObjects.push(object);
+    }
 
-    // example - function to check if an object is colliding with collidable objects
-    // checkCollision(object) {
-    //     // loop over all the other collidable objects 
-    //     this.collidableObjects.forEach(otherObject => {
-    //         // do a check to see if we have collided, if we have we can call object.onCollide(otherObject) which will
-    //         // call the onCollide we define for that specific object. This way we can handle collisions identically for all
-    //         // objects that can collide but they can do different things (ie. player colliding vs projectile colliding)
-    //         // use the modeling transformation for object and otherObject to transform position into current location
-    //     });
-    // }
+    createItemCollider(object, radius, onCollide = null) {
+        object.collider = {
+            type: "ITEM",
+            radius: radius,
+            onCollide: onCollide ? onCollide : (otherObject) => {
+                console.log("Item Picked Up");
+                let mapKeys = Object.keys(this.state.map);
+                let randomKey = mapKeys[Math.floor(mapKeys.length * Math.random())].split(",");
+                object.model.position = vec3.fromValues(parseInt(randomKey[0]), 0, parseInt(randomKey[1]));
+
+                otherObject.grow = true;
+                vec3.scale(otherObject.model.scale, otherObject.model.scale, 1.2);
+            }
+        };
+        this.collidableObjects.push(object);
+    }
+
+    checkCollision(object) {
+        // loop over all the other collidable objects 
+        this.collidableObjects.forEach(otherObject => {
+
+            if(object != otherObject && vec3.distance(object.model.position, otherObject.model.position) < (object.collider.radius + otherObject.collider.radius)){
+                object.collider.onCollide(otherObject);
+            }
+                
+        });
+    }
+
+    kill(train){
+        if(train.next){
+            let next = this.getObject(train.next);
+            next.previous = null;
+        }
+        train.alive = false;
+        vec3.add(train.model.position, train.model.position, vec3.fromValues(0.0,-5.0,0.0));
+
+        while(train.previous != null){
+            train = this.getObject(train.previous);
+            vec3.add(train.model.position, train.model.position, vec3.fromValues(0,-5,0));
+            train.alive = false;
+        }
+    }
 
     // runs once on startup after the scene loads the objects
     async onStart() {
@@ -58,7 +96,7 @@ class Game {
 
         console.log("Attaching trains");
         this.attachTrains();
-        console.log(this.state);
+        //console.log(this.state);
 
         this.player = this.state.trains[0]; //No train selection yet.
 
@@ -76,13 +114,6 @@ class Game {
         camera.position = vec3.fromValues(0, 35.0, 0);
         camera.up = vec3.fromValues(0, 0, -1);
         camera.front = vec3.fromValues(0, -35.0, 0);
-
-        // example - create sphere colliders on our two objects as an example, we give 2 objects colliders otherwise
-        // no collision can happen
-        // this.createSphereCollider(this.cube, 0.5, (otherObject) => {
-        //     console.log(`This is a custom collision of ${otherObject.name}`)
-        // });
-        // this.createSphereCollider(otherCube, 0.5);
 
         // example - setting up a key press event to move an object in the scene
         document.addEventListener("keydown", (e) => {
@@ -137,45 +168,6 @@ class Game {
             }
         });
 
-        this.customMethod(); // calling our custom method! (we could put spawning logic, collision logic etc in there ;) )
-
-        // example: spawn some stuff before the scene starts
-        // for (let i = 0; i < 10; i++) {
-        //     for (let j = 0; j < 10; j++) {
-        //         for (let k = 0; k < 10; k++) {
-        //             spawnObject({
-        //                 name: `new-Object${i}${j}${k}`,
-        //                 type: "cube",
-        //                 material: {
-        //                     diffuse: randomVec3(0, 1)
-        //                 },
-        //                 position: vec3.fromValues(4 - i, 5 - j, 10 - k),
-        //                 scale: vec3.fromValues(0.5, 0.5, 0.5)
-        //             }, this.state);
-        //         }
-        //     }
-        // }
-
-        // for (let i = 0; i < 10; i++) {
-        //     let tempObject = await spawnObject({
-        //         name: `new-Object${i}`,
-        //         type: "cube",
-        //         material: {
-        //             diffuse: randomVec3(0, 1)
-        //         },
-        //         position: vec3.fromValues(4 - i, 0, 0),
-        //         scale: vec3.fromValues(0.5, 0.5, 0.5)
-        //     }, this.state);
-
-
-        // tempObject.constantRotate = true; // lets add a flag so we can access it later
-        // this.spawnedObjects.push(tempObject); // add these to a spawned objects list
-
-        // tempObject.collidable = true;
-        // tempObject.onCollide = (object) => { // we can also set a function on an object without defining the function before hand!
-        //     console.log(`I collided with ${object.name}!`);
-        // };
-        // }
     }
 
     /**
@@ -189,7 +181,9 @@ class Game {
                 train.coords = [train.model.position[0], train.model.position[2]]; //save train x,z coords during tick
                 train.direction = this.direction.WEST; //should change when detecting other carts. Only the engine cart should have this
 
-
+                train.alive = true;
+                train.grow = false;
+                train.lastgrow= false;
                 train.last_rotation = train.model.rotation;
                 train.steerRotation = this.steer.FORWARD;
                 train.last_steer = this.steer.FORWARD;
@@ -197,6 +191,12 @@ class Game {
 
                 train.previous = null; //no other carts right now
                 this.state.trains.push(i); // more to move
+
+                this.createTrainCollider(train, 0.5);
+
+            }else if(train.name.includes("collectible") ){
+                this.createItemCollider(train, 0.5);
+                this.collectibles.push(i);
             }
         }
         for (let i = 0; i < this.state.trains.length; i++) {  //connect the rest of carts
@@ -280,16 +280,35 @@ class Game {
      * @param {int} trainIndex 
      */
     advanceTrain(trainIndex){
-        console.log("steering:");
-        console.log(this.getObject(this.player).steer);
+        //console.log("steering:");
+        //console.log(this.getObject(this.player).steer);
         let train = this.getObject(trainIndex);
+        //console.log(train);
 
-        console.log(train);
-        if(!train.name.includes("train")){
+        this.state.camera.last_pos = this.state.camera.position;
+
+        //console.log(train);
+        //console.log(trainIndex);
+        if(!train.name.includes("train") || !train.alive){
             return;
         }
+
+        if(train.grow){
+            if(!train.previous){
+                this.createNewCart(train);
+            }else{
+                train.lastgrow = true;
+                train.grow = false;
+                vec3.scale(train.model.scale, train.model.scale, 1.0/1.2);
+            }
+
+        }
         
-        let following = this.getObject(train.previous);
+        if(train.previous){
+            var following = this.getObject(train.previous);
+        }
+
+        
         train.last_coords = train.coords;
         this.rotateCart(train, 1);
         train.last_rotation = train.model.rotation;
@@ -346,13 +365,33 @@ class Game {
             this.updateCoords(train, this.steer.FORWARD);
         }
 
-        console.log("Advancing...");
-        console.log(train.last_coords);
-        console.log("to");
-        console.log(train.coords);
+        // console.log("Advancing...");
+        // console.log(train.last_coords);
+        // console.log("to");
+        // console.log(train.coords);
         
         //update the rest of the carts
+
+        if(!train.previous){
+            return;
+        }
+
         while(following != null){
+
+            if(train.lastgrow){
+                train.lastgrow = false;
+                following.lastgrow = following.grow;
+                following.grow = true;
+                vec3.scale(following.model.scale, following.model.scale, 1.2);
+            }else if(following.grow){
+                if(!following.previous){
+                    this.createNewCart(following);
+                }else{
+                    following.lastgrow = true;
+                    following.grow = false;
+                    vec3.scale(following.model.scale, following.model.scale, 1.0/1.2);
+                }
+            }
 
             following.last_rotation = following.model.rotation;
             following.model.rotation = train.last_rotation;
@@ -371,7 +410,48 @@ class Game {
             
         }
         
-        this.state.camera.last_pos = this.state.camera.position;
+        
+    }
+
+    async createNewCart(train){
+
+        const ID = this.state.loadObjects.length;
+        let templateId = 0;
+        this.state.trains.forEach(id => {
+            if(this.getObject(id).name == "oil_tank"){
+                templateId = id;
+            }
+        });
+
+        train.grow = false;
+        train.lastgrow = false;
+        vec3.scale(train.model.scale, train.model.scale, 1.0/1.2);
+
+        let newCart = {...this.state.loadObjects[templateId]};
+        newCart.name = "oil_tank" + ID.toString();
+        newCart.model = "oil_tank.obj";
+        newCart.position = train.model.position;
+        newCart.rotation = train.last_rotation;
+        newCart.material = train.material; //same color
+        this.state.loadObjects.push(newCart);
+        await addMesh(newCart);
+        train.previous = ID;
+        this.state.trains.push(ID);
+        newCart = this.FindByName("oil_tank" + ID.toString());
+        this.state.trains.forEach(id => {
+            if(this.getObject(id).name == train.name){
+                newCart.next = id;
+            }
+        });
+
+        newCart.alive = true;
+        newCart.grow = false;
+        newCart.lastgrow = false;
+        newCart.last_rotation = train.model.rotation;
+        newCart.steerRotation = train.last_steer
+        newCart.last_steer = this.steer.FORWARD;
+        newCart.previous = null; //no other carts right now
+        this.createTrainCollider(newCart, 0.5);
     }
 
     /**
@@ -381,9 +461,9 @@ class Game {
      * @returns 
      */
     updateCoords(train, steer){
-        console.log("updating to:");
-        console.log(steer);
-        console.log(this.steer.FORWARD);
+        // console.log("updating to:");
+        // console.log(steer);
+        // console.log(this.steer.FORWARD);
         let coords = train.coords;
         let direction = train.direction; 
         switch (direction) {
@@ -453,7 +533,6 @@ class Game {
                         break;
 
                     case this.steer.RIGHT:
-                        console.log("here");
                         train.coords = [coords[0] - 2, coords[1] - 2];
                         train.direction = this.direction.NORTH;
                         train.steerRotation = this.steer.RIGHT;
@@ -594,29 +673,38 @@ class Game {
         var tickProgress = (then.getTime() - this.tickStart)/this.tickLength;
         if(tickProgress >= 1){
             this.state.trains.forEach(trainIndex => {
-                console.log("Tick");
                 this.advanceTrain(trainIndex);
             });
             this.tickStart += this.tickLength;
             tickProgress -= 1;
+            console.log("Tick");
         }
 
         //move and interpolate rotation for each train
         this.state.trains.forEach(id => {
             let object = this.getObject(id);
-            object = this.moveCart(object, tickProgress);
-            this.rotateCart(object, tickProgress);
-            
+
+            if(object.alive){
+
+                object = this.moveCart(object, tickProgress);
+                this.rotateCart(object, tickProgress);
+                this.checkCollision(object);
+            }
         });
 
-        if (!this.perspective) {
+        this.collectibles.forEach(id => {
+            let object = this.getObject(id);
+            mat4.rotateY(object.model.rotation, object.model.rotation, deltaTime);
+            this.checkCollision(object);
+        });
+
+        if (!this.perspective && mainTrain.alive) {
             this.state.camera.up = vec3.fromValues(0,1,0);
             this.updateDefaultCameraPos(mainTrain);
             vec3.add(this.state.camera.position, mainTrain.model.position, this.defaultCameraPos);
             this.rotateCamera(mainTrain, tickProgress);
             vec3.subtract(this.state.camera.front, mainTrain.model.position, this.state.camera.position);
         }
-
         
     }
     
